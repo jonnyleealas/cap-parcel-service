258a2ebf6266515d2e02e9914111d34d7a7f2b4f
jonathon lee <jonnyleealas@gmail.com>
2020-10-07 00:13:37 -0700

    readmefix

--- caps.js
+++ caps.js
@@ -2,16 +2,22 @@
 'use strict';
 const events = require('./events');
 require('./vendor.js');
-// require('./drivers');
+require('./drivers.js');
+
 const chalk = require('chalk');
 
 
 //this logs things that are happening.
 events.on('log', payload => log(payload));
+events.on('order ready', orderReady);
+
+function orderReady(payload){
+  console.log('Order Ready For Pickup', payload);
+}
 
 function log(payload) {
   let time = new Date();
-  console.log('Events', {
+  console.log(`${chalk.bgYellow('EVENTS:')}`, {
     time,
   }, payload);
 }
--- hub.js
+++ hub.js
@@ -3,8 +3,3 @@
 const events = require('./events');
 const vender = require('./vendor');
 
-events.on('order ready', orderReady);
-
-function orderReady(payload){
-  console.log('Order Ready For Pickup', payload);
-}
