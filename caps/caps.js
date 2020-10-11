a638b44283d428a21b5c2a4d86fed01c3af86478
jonathon lee <jonnyleealas@gmail.com>
2020-10-11 01:30:57 -0700

    refactor

---
+++ caps/caps.js
@@ -0,0 +1,44 @@
+/* eslint-disable no-undef */
+'use strict';
+const events = require('../events/events');
+const chalk = require('chalk');
+require('../vendor/vendor.js');
+require('../drivers/drivers.js');
+
+// this logs things that are happening.
+events.on('delivered', payload => delivered(payload));
+// events.on('transic', payload+> logger )
+events.on('order ready', orderReady);
+events.on('log', payload => logger(payload));
+
+function orderReady(payload){
+  payload.event = 'Order Ready For Pickup';
+  events.emit('log', payload);
+  console.log('Order Ready For Pickup', payload);
+}
+
+function logger(payload) {
+  let time = new Date();
+  let results = {
+    time: time,
+    events: payload.event,
+    payload: payload,
+  };
+  console.log(`${chalk.bgCyan('Events:')}`,results.events, results.time);
+}
+
+function delivered(payload) {
+  console.log(`${chalk.bgBlue('Package Delivered:')}`, payload.orderId);
+}
+
+
+// function logger(events, payload){
+//   console.log('fuck', payload);
+//   let time = new Date();
+//   let obj = {
+//     time: time,
+//     events: events,
+//     payload: payload,
+//   };
+//   console.log('Event:', obj);
+// }
