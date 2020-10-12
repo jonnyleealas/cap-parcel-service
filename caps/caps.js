c5ba5e8e21f0a7a74f28475e8d9d0dbefbb6bceb
jonathon lee <jonnyleealas@gmail.com>
2020-10-11 21:54:48 -0700

    workingsomewhat

--- caps/caps.js
+++ caps/caps.js
@@ -1,28 +1,26 @@
 /* eslint-disable no-undef */
 'use strict';
-require('dotenv').config();
+
 const port = process.env.PORT || 8080;
 const io = require('socket.io')(port);
-const events = require('../events/events');
 const chalk = require('chalk');
-require('../vendor/vendor.js');
-require('../drivers/drivers.js');
+
 
 
 // Connect io server
 io.on('connection', (socket) =>{
   console.log(`connection on ${socket.id}`);
+  // this logs things that are happening.
+  socket.on('delivered', payload => delivered(payload));
+  socket.on('order ready', orderReady);
+  socket.on('log', payload => logger(payload));
+
 });
 
-// this logs things that are happening.
-// events.on('delivered', payload => delivered(payload));
-// events.on('transic', payload+> logger )
-// events.on('order ready', orderReady);
-// events.on('log', payload => logger(payload));
 
 function orderReady(payload){
   payload.event = 'Order Ready For Pickup';
-  // events.emit('log', payload);
+  io.emit('log', payload);
   console.log('Order Ready For Pickup', payload);
 }
 
@@ -40,14 +38,3 @@ function delivered(payload) {
   console.log(`${chalk.bgBlue('Package Delivered:')}`, payload.orderId);
 }
 
-
-// function logger(events, payload){
-//   console.log('fuck', payload);
-//   let time = new Date();
-//   let obj = {
-//     time: time,
-//     events: events,
-//     payload: payload,
-//   };
-//   console.log('Event:', obj);
-// }
